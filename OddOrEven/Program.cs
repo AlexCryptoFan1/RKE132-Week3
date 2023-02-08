b0521af727daa328a0f2408e683208206e1c1875
Aleksandr Utkin <utkin.aleksandr123@gmail.com>
2023-02-08 23:14:59 +0200

    add Week3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.WriteLine("enter number");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+// kas arv on paaris voi paaritu
+
+int result = userNum % 2;
+
+Console.WriteLine(result);
+
+if(result != 0)
+{
+    Console.WriteLine("number is odd"); 
+}    
+else
+{
+    Console.WriteLine("number is even");
+}    
